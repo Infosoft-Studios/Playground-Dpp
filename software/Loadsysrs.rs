1fac2cc735576c87d375fa22c75b56bec0a6b483
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-12 10:21:50 +0200

    Add Loadsysrs.rs with main and LoadMain functions

---
+++ software/Loadsysrs.rs
@@ -0,0 +1,15 @@
+fn main()
+{
+    let nodedata  = Box::new(90);
+    println!("nodedata:{nodedata}:");
+}
+
+fn LoadMain()
+{
+    let system = false;
+    let nodefunction = Box::new(100);
+    if (nodefunction != system)
+    {
+        println!("DataStructures rust");
+    }
+}
