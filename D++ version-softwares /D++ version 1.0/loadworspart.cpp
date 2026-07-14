b25cc27c965cf10e192de460a7a1fdfd10804eae
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-14 15:10:45 +0200

    Create loadworspart.cpp

---
+++ D++ version-softwares /D++ version 1.0/loadworspart.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include <cstdint>
+
+DWORD lodSysData = static_cast<void*> Loadbuffer();
+int main()
+{
+    WINCLASS wc;
+    int64_t                   = wc.reloaddatastruct(nullptr, true);
+  
+}
